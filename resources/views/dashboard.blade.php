469917bc8fe4f861e9d247b061a6872336740ad8
clemdevs <kimborisov93@gmail.com>
2025-03-28 13:31:55 +0200

    make link go to another tab

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
     <div class="grid grid-cols-3 gap-4 bg-white p-6 rounded-lg shadow-md">
         @foreach ($buttons as $button)
             <div class="flex items-center justify-center p-4 bg-gray-200 rounded-lg shadow">
-                <a href="{{ $button->link ?: route('button.edit', $button->id) }}"
+                <a href="{{ $button->link ?: route('button.edit', $button->id) }}" target="_blank"
                    class="w-16 h-16 flex items-center justify-center border-2 border-gray-400 rounded-full text-xl font-bold"
                    style="background-color: {{ $button->link ? $button->color : '#ffffff' }}">
                     {{ $button->link ? $button->title : '+' }}
